a57aea5025224c29acaa48823f535818223df46a
mahitha0404 <112369553+mahitha0404@users.noreply.github.com>
2023-08-22 09:25:01 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,130 @@
+
+
+/**
+ * Class student
+ */
+public class student {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void address;
+  private void dob;
+  private void subjects;
+  private void collage;
+  
+  //
+  // Constructors
+  //
+  public student () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of address
+   * @param newVar the new value of address
+   */
+  private void setAddress (void newVar) {
+    address = newVar;
+  }
+
+  /**
+   * Get the value of address
+   * @return the value of address
+   */
+  private void getAddress () {
+    return address;
+  }
+
+  /**
+   * Set the value of dob
+   * @param newVar the new value of dob
+   */
+  private void setDob (void newVar) {
+    dob = newVar;
+  }
+
+  /**
+   * Get the value of dob
+   * @return the value of dob
+   */
+  private void getDob () {
+    return dob;
+  }
+
+  /**
+   * Set the value of subjects
+   * @param newVar the new value of subjects
+   */
+  private void setSubjects (void newVar) {
+    subjects = newVar;
+  }
+
+  /**
+   * Get the value of subjects
+   * @return the value of subjects
+   */
+  private void getSubjects () {
+    return subjects;
+  }
+
+  /**
+   * Set the value of collage
+   * @param newVar the new value of collage
+   */
+  private void setCollage (void newVar) {
+    collage = newVar;
+  }
+
+  /**
+   * Get the value of collage
+   * @return the value of collage
+   */
+  private void getCollage () {
+    return collage;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void form_filling()
+  {
+  }
+
+
+  /**
+   */
+  public void regestration()
+  {
+  }
+
+
+}
