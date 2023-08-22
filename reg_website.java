2fb115e6ff6ef904aa8aaf93c7850618482f45f7
mahitha0404 <112369553+mahitha0404@users.noreply.github.com>
2023-08-22 09:24:14 +0530

    Add files via upload

---
+++ reg_website.java
@@ -0,0 +1,120 @@
+
+
+/**
+ * Class reg_website
+ */
+public class reg_website {
+
+  //
+  // Fields
+  //
+
+  private void qualification;
+  private void login_details;
+  private void fee_details;
+  private void exam_details;
+  
+  //
+  // Constructors
+  //
+  public reg_website () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of qualification
+   * @param newVar the new value of qualification
+   */
+  private void setQualification (void newVar) {
+    qualification = newVar;
+  }
+
+  /**
+   * Get the value of qualification
+   * @return the value of qualification
+   */
+  private void getQualification () {
+    return qualification;
+  }
+
+  /**
+   * Set the value of login_details
+   * @param newVar the new value of login_details
+   */
+  private void setLogin_details (void newVar) {
+    login_details = newVar;
+  }
+
+  /**
+   * Get the value of login_details
+   * @return the value of login_details
+   */
+  private void getLogin_details () {
+    return login_details;
+  }
+
+  /**
+   * Set the value of fee_details
+   * @param newVar the new value of fee_details
+   */
+  private void setFee_details (void newVar) {
+    fee_details = newVar;
+  }
+
+  /**
+   * Get the value of fee_details
+   * @return the value of fee_details
+   */
+  private void getFee_details () {
+    return fee_details;
+  }
+
+  /**
+   * Set the value of exam_details
+   * @param newVar the new value of exam_details
+   */
+  private void setExam_details (void newVar) {
+    exam_details = newVar;
+  }
+
+  /**
+   * Get the value of exam_details
+   * @return the value of exam_details
+   */
+  private void getExam_details () {
+    return exam_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void verefication()
+  {
+  }
+
+
+  /**
+   */
+  public void processing()
+  {
+  }
+
+
+  /**
+   */
+  public void regestration()
+  {
+  }
+
+
+}
