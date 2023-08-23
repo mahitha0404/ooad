8c28781a5aeb1b0c3f26cdf82a4af8393785c401
mahitha0404 <112369553+mahitha0404@users.noreply.github.com>
2023-08-23 09:06:39 +0530

    Add files via upload

---
+++ exp 33/registered_user.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * Class registered_user
+ */
+public class registered_user {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public registered_user () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+}
