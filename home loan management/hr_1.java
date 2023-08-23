bc5ff442fa1bd88f07d5ec09ef2a2c5d27de88dd
mahitha0404 <112369553+mahitha0404@users.noreply.github.com>
2023-08-23 09:04:51 +0530

    Add files via upload

---
+++ home loan management/hr_1.java
@@ -0,0 +1,69 @@
+
+
+/**
+ * Class hr_1
+ */
+public class hr_1 {
+
+  //
+  // Fields
+  //
+
+  private void check_details;
+  
+  //
+  // Constructors
+  //
+  public hr_1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of check_details
+   * @param newVar the new value of check_details
+   */
+  private void setCheck_details (void newVar) {
+    check_details = newVar;
+  }
+
+  /**
+   * Get the value of check_details
+   * @return the value of check_details
+   */
+  private void getCheck_details () {
+    return check_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void loss_of_pay()
+  {
+  }
+
+
+  /**
+   */
+  public void tax()
+  {
+  }
+
+
+  /**
+   */
+  public void hr1()
+  {
+  }
+
+
+}
